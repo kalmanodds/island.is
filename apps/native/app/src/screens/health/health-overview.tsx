7f9d31188940c6f570695298950664930db3c1f6
thoreyjona <thoreyjona@gmail.com>
2024-10-15 15:24:55 +0000

    fix: improve type safety and null checks

--- apps/native/app/src/screens/health/health-overview.tsx
+++ apps/native/app/src/screens/health/health-overview.tsx
@@ -124,17 +124,19 @@ export const HealthOverviewScreen: NavigationFunctionComponent = ({
   })
 
   const isOrganDonor =
-    organDonationRes.data?.healthDirectorateOrganDonation.donor?.isDonor
+    organDonationRes.data?.healthDirectorateOrganDonation.donor?.isDonor ??
+    false
 
   const isOrganDonorWithLimitations =
     isOrganDonor &&
-    organDonationRes.data?.healthDirectorateOrganDonation.donor?.limitations
-      ?.hasLimitations
+    (organDonationRes.data?.healthDirectorateOrganDonation.donor?.limitations
+      ?.hasLimitations ??
+      false)
 
   const organLimitations = isOrganDonorWithLimitations
     ? organDonationRes.data?.healthDirectorateOrganDonation.donor?.limitations?.limitedOrgansList?.map(
         (organ) => organ.name,
-      )
+      ) ?? []
     : []
 
   useConnectivityIndicator({
